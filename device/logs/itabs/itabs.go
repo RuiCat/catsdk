5a75fb8484bb537945a291fb2a8ff8da364723ea
RuiCat <littleruicat@qq.com>
2025-01-11 14:21:43 +0800

    获取符号表信息

--- device/logs/itabs/itabs.go
+++ device/logs/itabs/itabs.go
@@ -7,6 +7,7 @@ import (
 )
 
 var FtabMap = map[string]*runtime.Func{}
+var RtypeMap = map[string]reflect.Type{}
 
 func init() {
 	pc := make([]uintptr, 255)
@@ -14,12 +15,31 @@ func init() {
 	frame, ok := frames.Next()
 	if !ok {
 		funcInfo := reflect.ValueOf(frame).FieldByName("funcInfo")
-		datap := funcInfo.FieldByName("datap")
-		ftab := datap.Elem().FieldByName("ftab")
-		pclntable := datap.Elem().FieldByName("pclntable").Pointer()
+		datap := funcInfo.FieldByName("datap").Elem()
+		ftab := datap.FieldByName("ftab")
+		pclntable := datap.FieldByName("pclntable").Pointer()
 		for i := range ftab.Len() {
 			funcPtr := (*runtime.Func)(unsafe.Pointer(pclntable + uintptr(ftab.Index(i).FieldByName("funcoff").Uint())))
 			FtabMap[funcPtr.Name()] = funcPtr
 		}
+		// 数据底层类型
+		rtype := reflect.TypeOf(reflect.TypeOf(0)).Elem()
+		// 获取类型
+		types := uintptr(datap.FieldByName("types").Uint())
+		typelinks := datap.FieldByName("typelinks")
+		for i := range typelinks.Len() {
+			typeAt := reflect.NewAt(rtype, unsafe.Pointer(types+uintptr(typelinks.Index(i).Int()))).Type()
+			RtypeMap[typeAt.Name()] = typeAt
+		}
 	}
+}
+
+func GetFunc[T any](name string) *T {
+	ftab, ok := FtabMap[name]
+	if ok {
+		entry := ftab.Entry()
+		ptr := &entry
+		return (*T)(unsafe.Pointer(&ptr))
+	}
+	return nil
 }
